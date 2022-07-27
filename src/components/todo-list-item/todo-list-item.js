b422dd9545cc22f140c3cfc9e476241622e984d6
Roman Chuchev <romanok20@gmail.com>
2022-07-27 13:50:45 +0300

    Add setState

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,32 +2,58 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+  
+ 
+  state = {
+    done: false,
+    important: false
+  };
+
+  onLableClick = () => {
+    this.setState({
+      done:  true 
+    })
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important:  true 
+    })
+  };
 
   render() {
-    const { label, important = false } = this.props;
+  const { label } = this.props;
+  const { done, important } = this.state;
 
-    const style = {
-      fontWeight: important ? 'bold' : 'normal'
-    };
+  let classNames = 'todo-list-item'
+  if (done) {
+    classNames += ' done'
+  }
 
-    return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}>
-          {label}
-        </span>
+  if (important) {
+    classNames += ' important'
+  }
 
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation" />
-        </button>
-
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
-        </button>
+  return (
+    <span className={classNames}>
+      <span
+        className="todo-list-item-label"
+        onClick={ this.onLableClick }
+        >
+        {label}
       </span>
-    );
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={this.onMarkImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
   };
 };
