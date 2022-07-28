4979e7dd7c4008f9923acc958c61355af611256a
Roman Chuchev <romanok20@gmail.com>
2022-07-28 19:58:58 +0300

    Add input

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,18 +2,46 @@ import React, { Component } from "react";
 import "./item-add-form.css"
 
 export default class ItemAddForm extends Component {
+   
+   state = {
+      lable: ''
+   }
+
+   onLableChange = (event) => {
+      this.setState({
+         lable: event.target.value
+      })
+   }
+
+   onSubmit = (event) => {
+      event.preventDefault();
+      if (this.state.lable === '') return
+      this.props.onAddItem(this.state.lable)
+      this.setState({
+         lable: ''
+      })
+   }
+
    render() {
       return (
-      <div className="list-group-item form">
-         <div className="item-add-form">
+         <form className="item-add-form d-flex"
+               onSubmit={this.onSubmit}>
+
+            <input 
+               type="text"
+               className="form-control"
+               onChange={this.onLableChange}
+               placeholder="What needs to be done"
+               value={this.state.lable}
+            />
             <button  
                type="button"
                className="btn btn-info"
-               onClick={() => this.props.onAddItem('New Task') } >
+               onClick={this.onSubmit} >
                Add item
             </button>
-         </div>
-      </div>
-      )
-   }
-}
+
+         </form>
+      );
+   };
+};
